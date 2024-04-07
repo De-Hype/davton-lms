1c4ebc7fa3d1de85456b6da74195260e2b6fe092
David Hype <iamsocialhype@gmail.com>
2024-04-07 14:00:19 -0700

    Worked on the home page component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
 import HeaderTwo from "../Components/HeaderTwo";
 import logoTwo from "../assets/logoTwo.svg";
+import homeCoursesImg from "../assets/homeCoursesImg.svg";
+import homeComVid from "../assets/homeComVid.svg";
 import { FaCircleInfo } from "react-icons/fa6";
 const Home = () => {
   return (
     <div className="">
       <HeaderTwo />
+      <div className="bg-black">
+        
+
       <div className="relative flex items-center h-[90vh] hero-image">
         <div className="pl-6 w-[450px] text-white flex flex-col gap-3">
           <img className="h-8 w-8" src={logoTwo} alt="" />
           <h4 className="font-semibold">PROJECT MANAGEMENT</h4>
           <h2 className="text-3xl font-black">THE ESSENTIAL</h2>
-          <p className="">
+          <p className="text-sm font-light">
             Understand your audience, envision a creative solution, and test
             your prototype by Cooper Professional Education | By Cooper
             Professional Education
@@ -30,6 +35,29 @@ const Home = () => {
             </button>
           </div>
         </div>
+      </div>
+      <div className="">
+        <div className="py-2">
+          <h3 className="px-6 text-white font-bold py-1">Courses In Progress</h3>
+          <div className="flex overflow-x-auto items-center gap-1 px-6">
+            <img src={homeCoursesImg} alt="" />
+            <img src={homeCoursesImg} alt="" />
+            <img src={homeCoursesImg} alt="" />
+            <img src={homeCoursesImg} alt="" />
+            <img src={homeCoursesImg} alt="" />
+            {/* <img src={homeCoursesImg} alt="" /> */}
+          </div>
+        </div>
+        <div className="py-2">
+          <h3 className="px-6 text-white font-bold py-1">Company Videos</h3>
+          <div className="flex overflow-x-auto items-center gap-1 px-6">
+            <img src={homeComVid} alt="" />
+            <img src={homeComVid} alt="" />
+            <img src={homeComVid} alt="" />
+            <img src={homeComVid} alt="" />
+          </div>
+        </div>
+      </div>
       </div>
     </div>
   );
